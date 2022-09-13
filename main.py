6a1202294329b94287b9d854fbc25a9b90747fb1
Chillato <102484149+Chillato@users.noreply.github.com>
2022-09-13 05:58:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ try:
     import time
     from pyrogram import *
     from pyrogram.types import *
-    import asyncio, requests
+    import requests
 
     api_id = 8
     api_hash = ""
     bot = ""
 
-    canale = -111 # id canale
+    channel = -111 # id canale
 
     b = requests.get("https://blockchain.info/ticker").json()
     dollar = b["USD"]["last"]
-    minuti = 104
+    minutes = 104
 
     client = Client("btcsourceclone", api_id, api_hash, bot_token=bot)
     client.start()
 
     while True:
-        client.send_message(canale, f"{dollar}$")
-        time.sleep(int(minuti))
+        client.send_message(channel, f"{dollar}$")
+        time.sleep(int(minutes))
 except KeyboardInterrupt:
-    exit("Script fermato con successo...")
+    exit("Script successfully stopped ...")
 
